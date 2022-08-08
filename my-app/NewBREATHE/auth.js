96159ffe8863fd894ad8c4de796338a753e26506
Arinpe <dehtolah@gmail.com>
2022-08-08 22:49:55 +0100

    Change hero text

--- my-app/NewBREATHE/auth.js
+++ my-app/NewBREATHE/auth.js
@@ -4,7 +4,7 @@ import { Redirect, Route } from 'react-router-dom';
 // Utils
 import auth from '../../utils/auth';
 
-const Privat    eRoute = ({ component: Component, ...rest }) => (
+const PrivateRoute = ({ component: Component, ...rest }) => (
   <Route {...rest} render={props => (
     auth.getToken() !== null ? (
       <Component {...props} />
@@ -17,5 +17,19 @@ const Privat    eRoute = ({ component: Component, ...rest }) => (
     ):
   )} />
 );
+
+componentDidMount() {
+    // Generate the form with a function to avoid code duplication
+    // in other lifecycles
+    this.generateForm(this.props);
+  }
+
+  componentWillReceiveProps(nextProps) {
+    // Since we use the same container for all the auth views we need to update
+    // the UI on location change
+    if (nextProps.location.match.params.authType !== this.props.location.match.params.authType) {
+      this.generateForm(nextProps);
+    }
+  }
 
 export default PrivateRoute;
